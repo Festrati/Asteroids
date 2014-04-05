31247a4b45fb739ee595b38262861a15bbcc528a
festrati <festrati@gmail.com>
2014-04-05 00:56:46 -0300

    Add scripts and images

---
+++ Assets/Scripts/MoveMeteorScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMeteorScript : MonoBehaviour {
+
+	//velocidade
+	public Vector2 speed;
+	//direcao
+	public Vector2 direction;
+
+	public GameObject asteroid;
+
+	void Start()
+	{
+		speed = new Vector2 (1, 1);
+		direction = new Vector2(Random.Range(-5.0F, 10.0F), Random.Range(-5.0F, 10.0F));
+
+	}	
+	private Vector2 movement;
+		
+	void Update()
+	{
+		// 2 - Movement
+		movement = new Vector2(	speed.x * direction.x,
+								speed.y * direction.y);
+		}
+		
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
---
+++ Assets/Scripts/PlayerShipScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShipScript : MonoBehaviour {
+	// adicionando veocidade a nave
+	// instanciando
+	public Vector2 speed = new Vector2(20, 20);
+
+	// salvando o movimento
+	private Vector2 movement;
+	
+	// Update is called once per frame
+	void Update () {
+		// pegando informação dos eixos
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		// direções
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate(){
+		// movendo o objeto, não entendi ainda direito essa parada
+		rigidbody2D.velocity = movement;
+	}
+}
